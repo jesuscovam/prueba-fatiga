efd80bf6da1d340373699fd37e1f689d7b888c93
Jesus Cova <jesuscovam@gmail.com>
2025-05-26 19:20:10 -0600

    segundo commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Bienvenido al proyecto demo de fátima</h1>
+<button>ok no</button>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
